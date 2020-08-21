502c3ad9be9566df4622d9d188a39f44979eeb18
olmo <olmo1995@gmail.com>
2020-08-21 16:05:33 +0200

    feat: replacing index.js with server.js

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import { addTrip } from "./js/app.js"
+import { checkInput } from "./js/checkInput.js"
+import { addTripEvList } from "./js/app.js"
+import './styles/style.scss'
+
+export { addTrip }
+export { checkInput }
+export { addTripEvList }
