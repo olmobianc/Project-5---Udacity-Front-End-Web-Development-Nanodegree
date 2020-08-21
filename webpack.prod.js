542083d040d30cd3e2ceacb8ed667be596ffc915
olmo <olmo1995@gmail.com>
2020-08-21 15:32:16 +0200

    feat: finished to setting up webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 
 module.exports = {
@@ -12,13 +15,28 @@ module.exports = {
             test: '/\.js$/',
             exclude: /node_modules/,
             loader: "babel-loader"
+                },
+                {
+                    test: /\.scss$/,
+                    use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
                 }
         ]
     },
+
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
